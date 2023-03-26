860c1d220c30561e6a937401f98c448df74cd06e
gcaino <german.caino@hotmail.com>
2023-03-26 04:46:36 -0300

    build: add precompiled headers

---
+++ src/pch.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/* C Standard Library */
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+/* C++ Standar Library */
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <stack>
+#include <string>
+#include <vector>
+
+/* External Libraries */
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <SFML\Audio.hpp>
+
+/* My Files */
+#include "Aliases.h"
+#include "Constants.h"
